99624e95dba7c09899b384b3dba170e41d43aaeb
r-iradukunda <r.iradukund@alustudent.com>
2023-12-14 10:06:10 -0800

    The sky is the limit not pp

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,11 @@
-# Test nginx server setup
+# fix  nginx failed requests
 
-exec { 'ULIMIT':
-  onlyif  => 'test -e /etc/default/nginx',
-    command => 'sed -i "5s/[0-9]\+/$( ulimit -n )/" /etc/default/nginx; service nginx restart',
-      path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
-      }
+exec { 'fix--for-nginx':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
+
+-> exec { 'nginx-restart':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
+}
